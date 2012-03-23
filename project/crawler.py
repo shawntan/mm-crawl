afb0da64bec0d5a3f9784c2eeb8b913d042b3104
Shawn Tan <shawn@wtf.sg>
2012-03-23 15:23:04 +0800

    Included going back function

--- project/crawler.py
+++ project/crawler.py
@@ -3,30 +3,39 @@ from browser import *
 from urlparse import urljoin
 from features import *
 from reward import *
-import sys
-link_queue = []
+import sys,random
 visited = set()
+history_stack = []
 def queue_processor(self,curr_url,document):
+	print curr_url
+	print history_stack
+	visited.add(curr_url)
 	try:
 		anchors = document.findAll("a")
+		link_queue = []
 		for a in anchors:
 			try:
 				extract_features(a.previousSibling(),a,a.nextSibling())
-				href = urljoin(curr_url,str(a.attribute("href")))
-				if href not in visited and href.find("ycombinator.com"):
+				url = str(a.attribute("href")).split('#')[0]
+				href = urljoin(curr_url,url)
+				if href not in visited\
+						and href.find("ycombinator.com") >= 0\
+						and not href.endswith("jpg")\
+						and not href.endswith("gif")\
+						and not href.endswith("png"):
 					link_queue.append(href)
-					visited.add(href)
 			except Exception as ex:
 				print ex
-
-	
-		print reward(document)
 		if link_queue:
+			print reward(document)
+			random.shuffle(link_queue)
 			link = link_queue.pop(0)
-			print link
+			history_stack.append(curr_url)
 			self.load(link)
 		else:
-			sys.exit()
+			if history_stack:
+				self.load(history_stack.pop())
+			else: sys.exit()
 	except Exception as ex:
 		print ex
 		sys.exit()
